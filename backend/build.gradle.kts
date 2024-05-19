07057e8c5bb1dec67127965d3107125cc38d4273
Pham Minh Hoang Henry <minhhoangpham@live.de>
2024-05-19 17:23:10 +0200

    feature: add swagger

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -24,6 +24,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")
 
 	implementation("org.liquibase:liquibase-core:4.27.0")
 
