9951cfdbf479bd12ebe95d1d3394053456e30e5d
Minzii <phattaree.nuchna@hotmail.com>
2025-01-26 19:18:56 +0100

    feature add delete find passwordbyID

--- backend/src/main/kotlin/charity/aco/guardian/password/api/PasswordApi.kt
+++ backend/src/main/kotlin/charity/aco/guardian/password/api/PasswordApi.kt
@@ -2,8 +2,10 @@ package charity.aco.guardian.password.api
 
 import charity.aco.guardian.password.dto.PasswordDto
 import charity.aco.guardian.password.service.PasswordService
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import java.util.*
 
 @RestController
 @RequestMapping("/password")
@@ -19,5 +21,17 @@ class PasswordApi(private val passwordService: PasswordService) {
     fun createPassword(@RequestBody passwordDto: PasswordDto): ResponseEntity<PasswordDto> {
         val result = this.passwordService.createPassword(passwordDto)
         return ResponseEntity.ok(result)
+    }
+
+    @GetMapping("/{passwordId}")
+    fun getById(@PathVariable passwordId: UUID): ResponseEntity<PasswordDto> {
+        val result = this.passwordService.findById(passwordId)
+        return ResponseEntity.ok(result)
+    }
+
+    @DeleteMapping("/{passwordId}")
+    fun deleteEvent(@PathVariable passwordId: UUID): ResponseEntity<Void> {
+        this.passwordService.deletePassword(passwordId)
+        return ResponseEntity(HttpStatus.OK)
     }
 }
--- backend/src/main/kotlin/charity/aco/guardian/password/converter/PasswordConverter.kt
+++ backend/src/main/kotlin/charity/aco/guardian/password/converter/PasswordConverter.kt
@@ -11,46 +11,6 @@ import javax.crypto.Cipher
 import javax.crypto.IllegalBlockSizeException
 import javax.crypto.spec.SecretKeySpec
 
-/*
-
-import jakarta.persistence.AttributeConverter
-import org.springframework.beans.factory.annotation.Value
-import org.springframework.stereotype.Component
-import java.security.Key
-import java.util.*
-import javax.crypto.Cipher
-import javax.crypto.spec.SecretKeySpec
-
-
-@Component
-class PasswordConverter : AttributeConverter<String, String> {
-
-    private val AES: String = "AES"
-
-    private var key: Key? = null
-    private var cipher: Cipher = Cipher.getInstance(AES)
-
-    @Value("\${guardian.psskee}")
-    lateinit var SECRET: String
-
-        init {
-        key = SecretKeySpec(SECRET.toByteArray(), AES)
-        cipher = Cipher.getInstance(AES)
-    }
-
-
-    override fun convertToDatabaseColumn(attribute: String?): String {
-        key = SecretKeySpec(SECRET.toByteArray(), AES)
-        cipher!!.init(Cipher.ENCRYPT_MODE, key)
-        return Base64.getEncoder().encodeToString(cipher!!.doFinal(attribute!!.toByteArray()))
-    }
-
-    override fun convertToEntityAttribute(dbData: String?): String {
-        key = SecretKeySpec(SECRET.toByteArray(), AES)
-        cipher!!.init(Cipher.ENCRYPT_MODE, key)
-        return Base64.getEncoder().encodeToString(cipher!!.doFinal(dbData!!.toByteArray()))
-    }
-}*/
 
 @Component
 class PasswordConverter : AttributeConverter<String, String> {
