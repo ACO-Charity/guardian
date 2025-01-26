227728e71998ecf083e0b26536c768146c988836
Minzii <phattaree.nuchna@hotmail.com>
2025-01-26 19:18:56 +0100

    feature add delete find passwordbyID

--- backend/src/main/kotlin/charity/aco/guardian/password/service/PasswordService.kt
+++ backend/src/main/kotlin/charity/aco/guardian/password/service/PasswordService.kt
@@ -4,6 +4,7 @@ import charity.aco.guardian.password.domain.Password
 import charity.aco.guardian.password.dto.PasswordDto
 import charity.aco.guardian.password.repository.PasswordRepository
 import org.springframework.stereotype.Service
+import java.util.*
 
 @Service
 class PasswordService(private val passwordRepository: PasswordRepository) {
@@ -18,4 +19,14 @@ class PasswordService(private val passwordRepository: PasswordRepository) {
         val saved = this.passwordRepository.save(toSave)
         return saved.toDto()
     }
+
+    fun findById(passwordId: UUID): PasswordDto {
+        val password = this.passwordRepository.findById(passwordId).orElseThrow { RuntimeException("No Password found for ${passwordId}")}
+        return password.toDto()
+    }
+    fun deletePassword(passwordId: UUID) {
+        this.passwordRepository.deleteById(passwordId)
+    }
+
+
 }
